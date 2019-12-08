1d211efff68484918775468c83c9ae05c1ce838f
Nate Bennett <32677820+natefbennett@users.noreply.github.com>
2019-12-07 19:18:17 -0500

    big commit: save keybinding to object, serialize special keys, small UI changes, reset to default broken

--- src/cascadia/TerminalApp/SettingsPage.h
+++ src/cascadia/TerminalApp/SettingsPage.h
@@ -15,8 +15,13 @@ namespace winrt::TerminalApp::implementation
     public:
         SettingsPage();
         SettingsPage(std::shared_ptr<::TerminalApp::CascadiaSettings> settings);
-        winrt::hstring GetKeybindingString(int32_t var);
-        void actionSelected(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::Controls::SelectionChangedEventArgs const& e);
+
+        winrt::hstring GetKeybindingString(const int32_t actionIndex);
+        hstring VkeyToHstring(winrt::Windows::System::VirtualKey const vkey);
+
+        void ActionSelected(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::Controls::SelectionChangedEventArgs const& e);
+        void PreviewKeyDownHandler(winrt::Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::Input::KeyRoutedEventArgs const& args);       
+        void PreviewKeyUpHandler(winrt::Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::Input::KeyRoutedEventArgs const& args);
         void SaveKeybindings(winrt::Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
         void ResetToDefault(winrt::Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
 
@@ -24,6 +29,7 @@ namespace winrt::TerminalApp::implementation
         
     private:
         std::shared_ptr<::TerminalApp::CascadiaSettings> _settings{ nullptr };
+        winrt::hstring _buffer;
     };
 }
 
